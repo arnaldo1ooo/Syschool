a89408db5ae1e9d074ef36cc3628eaee8b8a9b3a
arnaldorcm <arnaldo1ooo95@gmail.com>
2022-12-10 12:51:46 -0300

    Se agrega directorio de sqls, se altera version de script a V2 ya que el V1 es el que crea la tabla de flyway historic

--- src/dao/FlywayMetodos.java
+++ src/dao/FlywayMetodos.java
@@ -12,15 +12,14 @@ import org.flywaydb.core.Flyway;
  * @author Arnaldo_Cantero
  */
 public class FlywayMetodos {
-    private String localizacionSQLs = "src/db/migration";
-
+    private String localizacionSQLs = "resources/db/migration";
 
     public void iniciarFlyway(String url, String user, String pass){
         Flyway flyway = org.flywaydb.core.Flyway
                 .configure()
-                .dataSource(url, user, pass) //url, user, pass
+                .dataSource(url, user, pass) //url, user, pass de la base
                 .locations(localizacionSQLs)
-                .baselineOnMigrate(true) //Para que genere la tabla flyway history
+                .baselineOnMigrate(true) //Para que genere la tabla flyway history, el siguiente script debe ser V2
                 .load();
         
         flyway.migrate();
